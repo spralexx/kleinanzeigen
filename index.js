d68b28a546c66d670f247d2b6756a1871f33733b
Rene Krick <rene.krick@gmx.de>
2017-05-25 23:36:54 +0200

    running

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var port = 3000; //port to listen on
+var app = express();
+
+var KleinanzeigenSchema = new mongoose.Schema({
+	text: String
+});
+
+var dbconn = mongoose.createConnection('mongodb://127.0.0.1/Kleinanzeigen'),
+	Anzeigen = dbconn.model('kleinanzeigen', KleinanzeigenSchema, 'kleinanzeigen');
+
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+app.use(bodyParser());
+
+
+    // Define routes.
+app.get('/',function(req, res) {
+  //console.log(req);
+	res.render('default');
+}
+  );
+app.post('/anzeigeaufgeben',function(req,res){
+	try{
+		if(req.body.kleinanzeige.length<=160){
+
+			console.log(req.body.kleinanzeige);
+			var anzeige = new Anzeigen({text: req.body.kleinanzeige});
+			anzeige.save(function (err) {
+				if (err) return console.error(err);
+			});
+			res.send('Anzeige gespeichert');
+		}else{
+			throw 'Text zu lang';
+		}
+	}catch(err){
+		console.log(err);
+		res.send(err);
+	}
+});
+
+app.listen(port, function() {
+	console.log('Listening on Port: ' + port);
+});
---
+++ public/js/client.js
@@ -0,0 +1,33 @@
+function init(){
+	var form = document.getElementById('kleinanzeige');
+	form.addEventListener('submit',function(e){
+		e.preventDefault();
+		if(document.getElementById('kleinanzeigentext').value){
+
+			var xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+
+					console.log(this.responseText);
+					var p = document.getElementById('status');
+					p.innerHTML=this.responseText;
+					if(this.responseText=='Anzeige gespeichert'){
+						setTimeout(function(){ location.reload();}, 1500);
+					}
+				}
+			};
+			xhttp.open('POST', '/anzeigeaufgeben', true);
+			xhttp.setRequestHeader('Content-Type', 'application/json');
+			xhttp.send(JSON.stringify({kleinanzeige:document.getElementById('kleinanzeigentext').value}));
+
+
+		}
+		else{
+			console.log('leere anzeige nicht gesendet');
+		}
+	});
+
+
+}
+
+window.addEventListener('load', init);
